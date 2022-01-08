1502633262c058195300e5a478923a9775ab4a83
Ariful Islam <arifulislam161024@gmail.com>
2022-01-08 23:50:15 +0600

    primary color palate added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-center">Bismillah</h1>
+      <h1 className="text-3xl text-center text-primary border  ">Bismillah</h1>
     </div>
   );
 }
